60e4c47d50d911033db0ea7cd4d312489f45a272
Camy~ <camyms13@gmail.com>
2023-04-27 12:24:05 +0200

    models et debut de la route tweet

---
+++ models/tweets.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const tweetSchema = mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
+  tweet: String,
+  date: Date,
+  like: [{ type: mongoose.Schema.Types.ObjectId, ref: "users" }],
+});
+
+const Tweet = mongoose.model("tweets", tweetSchema);
