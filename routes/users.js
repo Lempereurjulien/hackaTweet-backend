0f7c0d56305898684e8f7160f9de95b8326765d8
Camy~ <camyms13@gmail.com>
2023-04-27 11:56:27 +0200

    routes users signin et signup, models, connection, .env, app.js, ne pas oublier de yarn install ;)

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,56 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+require("../models/connection");
+const User = require("../models/users");
+const { checkBody } = require("../modules/checkBody");
+const uid2 = require("uid2");
+const bcrypt = require("bcrypt");
+
+router.post("/signup", (req, res) => {
+  // Vérifie que les champs soient correctement remplies
+  if (!checkBody(req.body, ["firstname", "username", "password"])) {
+    res.json({ result: false, error: "Missing or empty fields" });
+    return;
+  }
+
+  // Vérifie si l utilisateur est deja inscrit, s'il ne l'est pas, crée un nouvel utilisateur
+  User.findOne({ username: req.body.username }).then((data) => {
+    if (data === null) {
+      const hash = bcrypt.hashSync(req.body.password, 10);
+
+      const newUser = new User({
+        firstname: req.body.firstname,
+        username: req.body.username,
+        password: hash,
+        token: uid2(32),
+      });
+
+      newUser.save().then((newUser) => {
+        res.json({ result: true, token: newUser.token });
+      });
+    } else {
+      // L'utilisateur existe déja dans la BDD
+      res.json({ result: false, error: "User already exists" });
+    }
+  });
+});
+
+// Vérifie que les champs soient correctement remplies
+router.post("/signin", (req, res) => {
+  if (!checkBody(req.body, ["username", "password"])) {
+    res.json({ result: false, error: "Missing or empty fields" });
+    return;
+  }
+  // Vérifie si le username et le mot de passe correspondent a des données dans la BDD
+  User.findOne({ username: req.body.username }).then((data) => {
+    if (data && bcrypt.compareSync(req.body.password, data.password)) {
+      res.json({ result: true, token: data.token });
+    } else {
+      // Soit l utilisateur n existe pas, soit le mot de passe est faux, renvoie un message d erreur
+      res.json({ result: false, error: "User not found or wrong password" });
+    }
+  });
 });
 
 module.exports = router;
